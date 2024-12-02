4ff8d19e434e635be3c3c235adaf8e5cd2436052
sennaTia <1214998@student.roc-nijmegen.nl>
2024-12-02 11:36:57 +0100

    Update MasterMind2_0.java

--- src/masterMind2_0/MasterMind2_0.java
+++ src/masterMind2_0/MasterMind2_0.java
@@ -5,92 +5,95 @@ import java.util.Scanner;
 public class MasterMind2_0 {
 
 	public static void main(String[] args) {
-		String[] secretCode = { "R", "B", "G", "O" };
 
 		int i = 0;
 		int guesses = 0;
 		int score = 0;
 
-		    Scanner scanner= new Scanner(System.in);
-			String[] colors = { "R ", "G ", "B ", "Y ", "P ", "O ", };
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		
 
-			while (guesses < 10) {
-				i++;
-				System.out.println("Guess: " + (i) + "/10\n" + "Please choose 4 out of any of the 6 Colors: "
-						+ colors[0] + colors[1] + colors[2] + colors[3] + colors[4] + colors[5]);
-				String userCode[] = { scanner.next(), scanner.next(), scanner.next(), scanner.next() };
+		while (guesses < 10) {
+			i++;
+			System.out.println("Guess: " + (i) + "/10\n" + "Please choose 4 out of any of the 6 Colors: R, G, B, Y, P, O");
+			String[] secretCode = { "P", "O", "Y", "G" };
+			String userCode[] = { scanner.next(), scanner.next(), scanner.next(), scanner.next() };
 
-				if (userCode[0].equals(secretCode[0])) {
-					System.out.print("B ");
-					score++;
-				} else if (userCode[0].equals(secretCode[1])) {
-					System.out.print("W ");
-				} else if (userCode[0].equals(secretCode[2])) {
-					System.out.print("W ");
-				} else if (userCode[0].equals(secretCode[3])) {
-					System.out.print("W ");
-				} else {
-					System.out.print("- ");
-				}
-
-				if (userCode[1].equals(secretCode[1])) {
-					System.out.print("B ");
-					score++;
-				} else if (userCode[1].equals(secretCode[0])) {
-					System.out.print("W ");
-				} else if (userCode[1].equals(secretCode[2])) {
-					System.out.print("W ");
-				} else if (userCode[1].equals(secretCode[3])) {
-					System.out.print("W ");
-				} else {
-					System.out.print("- ");
-				}
-				
-				if (userCode[2].equals(secretCode[2])) {
-					System.out.print("B ");
-					score++;
-				} else if (userCode[2].equals(secretCode[1])) {
-					System.out.print("W ");
-				} else if (userCode[2].equals(secretCode[0])) {
-					System.out.print("W ");
-				} else if (userCode[2].equals(secretCode[3])) {
-					System.out.print("W ");
-				} else {
-					System.out.print("- ");
-				}
-
-				if (userCode[3].equals(secretCode[3])) {
-					System.out.print("B ");
-					score++;
-				} else if (userCode[3].equals(secretCode[1])) {
-					System.out.print("W ");
-				} else if (userCode[3].equals(secretCode[2])) {
-					System.out.print("W ");
-				} else if (userCode[3].equals(secretCode[0])) {
-					System.out.print("W ");
-				} else {
-					System.out.print("- ");
-				}
-
-				guesses++;
-
-				
-				
-				if (score == 4) {
-					guesses = 11;
-					break;
-				}
-				score = 0;
-
-			}
-			if (score == 4) {
-				System.out.println("\nYou have won, GG");
+			
+			int x = 0;
+			
+			if (userCode[x].equals(secretCode[x])) {
+				System.out.print("B ");
+				score++;
+			} else if (userCode[x].equals(secretCode[1])) {
+				System.out.print("W ");
+			} else if (userCode[x].equals(secretCode[2])) {
+				System.out.print("W ");
+			} else if (userCode[x].equals(secretCode[3])) {
+				System.out.print("W ");
 			} else {
-				System.out.println("\nYou have lost");
+				System.out.print("- ");
 			}
-			scanner.close();
-		 
+
+			++x;
+			if (userCode[x].equals(secretCode[x])) {
+				System.out.print("B ");
+				score++;
+			} else if (userCode[x].equals(secretCode[0])) {
+				System.out.print("W ");
+			} else if (userCode[x].equals(secretCode[2])) {
+				System.out.print("W ");
+			} else if (userCode[x].equals(secretCode[3])) {
+				System.out.print("W ");
+			} else {
+				System.out.print("- ");
+			}
+
+			++x;
+			if (userCode[x].equals(secretCode[x])) {
+				System.out.print("B ");
+				score++;
+			} else if (userCode[x].equals(secretCode[1])) {
+				System.out.print("W ");
+			} else if (userCode[x].equals(secretCode[0])) {
+				System.out.print("W ");
+			} else if (userCode[x].equals(secretCode[3])) {
+				System.out.print("W ");
+			} else {
+				System.out.print("- ");
+			}
+
+			++x;
+			if (userCode[x].equals(secretCode[x])) {
+				System.out.print("B ");
+				score++;
+			} else if (userCode[x].equals(secretCode[1])) {
+				System.out.print("W ");
+			} else if (userCode[x].equals(secretCode[2])) {
+				System.out.print("W ");
+			} else if (userCode[x].equals(secretCode[0])) {
+				System.out.print("W ");
+			} else {
+				System.out.print("- ");
+			}
+
+			guesses++;
+
+			if (score == 4) {
+				guesses = 11;
+				break;
+			}
+			score = 0;
+
+		}
+		if (score == 4) {
+			System.out.println("\nYou have won, GG");
+		} else {
+			System.out.println("\nYou have lost");
+		}
+		scanner.close();
+
 	}
 }
-
-	
